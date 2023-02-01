6d9f0e1161b482306b7e5be992bb9b0a0b9bfb15
Altaf Malik <altafmalik555786@gmail.com>
2023-02-01 22:34:57 +0500

    first column completed

--- src/components/menu-bar/index.tsx
+++ src/components/menu-bar/index.tsx
@@ -1,32 +1,27 @@
 import React from 'react'
-import style from './style.module.scss'
 import { Menu } from 'antd'
 import 'antd/dist/antd.css'
-import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { constRoute } from '../../utils/route/index'
 
+
 function MenuBar() {
+    const navigate = useNavigate()
     const itemsList = [
-        { label: 'item 1', key: 'item-1' },
-        { label: 'item 2', key: 'item-2' },
-        {
-            label: 'sub menu',
-            key: 'submenu',
-            children: [{ label: 'item 3', key: 'submenu-item-1' }],
-        },
+        { label: 'Home', key: constRoute.dashboard},
+        { label: 'My Profile', key: constRoute.userProfile},
     ]
+
+    const changeNavigate = (e) => {
+        navigate(e.key)
+    }
+
 
     return (
         <div style={{ height: '10vh' }}>
-            <NavLink className={style.Logo} to={constRoute.dashboard}>
-                Kodecolor
-            </NavLink>
-            <Menu mode="horizontal" items={itemsList}>
-                <NavLink className={style.Logo} to={constRoute.dashboard}>
-                    Kodecolor{' '}
-                </NavLink>
-                5
-            </Menu>
+            <Menu mode="horizontal" items={itemsList} onClick={(e) => {
+                changeNavigate(e)
+            }} />
         </div>
     )
 }
